8e78091539078c8ef417d0104e478449fcffd4e8
anatuo <599079204@qq.com>
2024-09-20 19:10:07 +0800

    测试

--- ljw.h
+++ ljw.h
@@ -6,13 +6,67 @@
 #define ZUOSHEN_LJW_H
 
 #include <bits/stdc++.h>
+#include <windows.h>
+#include <wincrypt.h>
 
 using namespace std;
+
+/*********************************** 随机数相关 **********************************/
+class WinRandom {
+    HCRYPTPROV handle;
+public:
+    WinRandom() {
+        handle = NULL;
+        CryptAcquireContext(
+                (HCRYPTPROV *) &handle, NULL, NULL,
+                PROV_RSA_FULL, 0
+        );
+    }
+
+    ~WinRandom() {
+        if (handle != NULL) CryptReleaseContext(handle, 0);
+    }
+
+    bool randBuf(void *dest, int len) {
+        if (handle == NULL) return false;
+        return CryptGenRandom(handle, len, (BYTE *) dest);
+    }
+
+#   define _(func, typ) \
+    typ func() { \
+        typ ret = 0; \
+        assert(randBuf((void *)&ret, sizeof(ret))); \
+        return ret; \
+    }
+
+    _(randInt, int)
+
+    _(randLong, long long)
+
+    _(randUnsigned, unsigned)
+
+    _(randUnsignedLong, unsigned long long)
+
+    _(randShort, short)
+
+    _(randUnsignedShort, unsigned short)
+
+    _(randChar, char)
+
+    _(randUnsignedChar, unsigned char)
+
+    _(randSignedChar, signed char)
+};
+
+WinRandom random;
+
+/*********************************** 随机数相关 **********************************/
 
 /**
  * 打印整型数组, 可指定每个元素的宽度和每行的数字等
  */
 void printArray(int array[], int size, int w = 0, int lineCount = 30) {
+
     for (int i = 0; i < size; ++i) {
         cout << setw(w) << array[i] << " ";
         if ((i + 1) % lineCount == 0) cout << endl;
@@ -24,10 +78,22 @@ void printArray(int array[], int size, int w = 0, int lineCount = 30) {
  */
 void generateArrayData(int array[], int size,
                        bool print = false, int minV = 0, int maxV = 99) {
-    srand(time(0));
     int modV = maxV - minV + 1;
-    for (int i = 0; i < size; ++i) array[i] = minV + rand() % modV;
+    for (int i = 0; i < size; ++i) array[i] = minV + random.randInt() % modV;
     if (print) printArray(array, size);
+}
+
+/**
+ * 判断输入的整数是否为素数
+ */
+bool isPrime(int value) {
+    if (value < 2) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    for (int i = 3, temp = sqrt(value); i <= temp; i += 2) {
+        if (value % i == 0) return false;
+    }
+    return true;
 }
 
 #endif //ZUOSHEN_LJW_H
---
+++ practice_6.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "ljw.h"
+
+using namespace std;
+
+// 在有序数组中查找值为value的位置, 若不存在返回-1, 若有多个返回任意一个即可
+int searchValue(int array[], int size, int value) {
+    int l = 0, r = size - 1, m;
+    while (l <= r) {
+        m = l + (r - l) / 2;
+        if (array[m] == value) return m;
+        if (array[m] > value) r = m - 1;
+        else l = m + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int size = 30;
+    int array[size];
+    generateArrayData(array, size, true);
+    sort(array, array + size);
+    printArray(array, size);
+    cout << searchValue(array, size, 55);
+
+    WinRandom a;
+    return 0;
+}
